cefa5d88d558253d29be1efec40c585ca0d5678d
Ben Lieblich <ben1787@gmail.com>
2015-10-21 17:12:23 -0400

    added the tableviewcontroller to choose members of a new group. Not pretty but the skeleton is there now

---
+++ Prototype/ZDT-DownloadVideo/FriendTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FriendTableViewCell.swift
+//  Prototype
+//
+//  Created by Ben Lieblich on 10/20/15.
+//  Copyright © 2015 Sztanyi Szabolcs. All rights reserved.
+//
+
+import UIKit
+
+class FriendTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
