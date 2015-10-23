1121c24610fea98962820b9a0af3ee578879ed77
yuchien <yc2257@cornell.edu>
2015-10-23 00:04:30 -0400

    When login success, post to backend to create account if user not exist

--- Prototype/ZDT-DownloadVideo/LoginViewController.swift
+++ Prototype/ZDT-DownloadVideo/LoginViewController.swift
@@ -34,19 +34,70 @@ class LoginViewController: UIViewController, FBSDKLoginButtonDelegate {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+
+    func postUser(){
+
     
-//    func returnUserData()
-//    {
-//        let graphRequest : FBSDKGraphRequest = FBSDKGraphRequest(graphPath: "me", parameters: nil)
-//        graphRequest.startWithCompletionHandler({ (connection, result, error) -> Void in
-//            
-//            if ((error) != nil) {
-//                print("Error: \(error)")
-//            } else {
-//                print("fetched user: \(result)")
-//            }
-//        })
-//    }
+    
+    }
+    
+    func returnUserData()
+    {
+        let graphRequest : FBSDKGraphRequest = FBSDKGraphRequest(graphPath: "me", parameters: nil)
+        graphRequest.startWithCompletionHandler({ (connection, result, error) -> Void in
+            
+            if ((error) != nil) {
+                print("Error: \(error)")
+            } else {
+                print("fetched user: \(result)")
+                
+                let request = NSMutableURLRequest(URL: NSURL(string: "http://refly-bd.herokuapp.com/api/register")!)
+                let session = NSURLSession.sharedSession()
+                request.HTTPMethod = "POST"
+                
+                let params = ["user_fb_id":result["id"] as! String, "user_fb_name":result["name"] as! String]
+                
+                request.HTTPBody = try? NSJSONSerialization.dataWithJSONObject(params, options: [])
+                
+                request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+                request.addValue("application/json", forHTTPHeaderField: "Accept")
+                
+                let task = session.dataTaskWithRequest(request, completionHandler: {data, response, error -> Void in
+                    print("Response: \(response)")
+                    let strData = NSString(data: data!, encoding: NSUTF8StringEncoding)
+                    print("Body: \(strData)")
+                    
+                    let json = try? NSJSONSerialization.JSONObjectWithData(data!, options: .MutableLeaves) as? NSDictionary
+                    
+                    // Did the JSONObjectWithData constructor return an error? If so, log the error to the console
+                    if(error != nil) {
+                        print(error!.localizedDescription)
+                        let jsonStr = NSString(data: data!, encoding: NSUTF8StringEncoding)
+                        print("Error could not parse JSON: '\(jsonStr)'")
+                    }
+                    else {
+                        // The JSONObjectWithData constructor didn't return an error. But, we should still
+                        // check and make sure that json has a value using optional binding.
+                        if let parseJSON = json {
+                            // Okay, the parsedJSON is here, let's get the value for 'success' out of it
+                            let success = parseJSON!["success"] as? Int
+                            print("Succes: \(success)")
+                        }
+                        else {
+                            // Woa, okay the json object was nil, something went worng. Maybe the server isn't running?
+                            let jsonStr = NSString(data: data!, encoding: NSUTF8StringEncoding)
+                            print("Error could not parse JSON: \(jsonStr)")
+                        }
+                    }
+                })
+                
+                task.resume()
+                return
+
+                
+            }
+        })
+    }
 
     
     func loginButton(loginButton: FBSDKLoginButton!, didCompleteWithResult result: FBSDKLoginManagerLoginResult!, error: NSError!) {
@@ -56,6 +107,7 @@ class LoginViewController: UIViewController, FBSDKLoginButtonDelegate {
         } else if result.isCancelled {
             // Handle cancellations
         } else {
+            self.returnUserData()
             self.dismissViewControllerAnimated(true, completion: nil)
         }
     }
