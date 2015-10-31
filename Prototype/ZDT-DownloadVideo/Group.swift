c4780119b42da29e85d1cb03d58e1f769702a136
brunnoattorre <brattorre@gmail.com>
2015-10-31 16:06:22 -0400

    Organizing packages

---
+++ Prototype/ZDT-DownloadVideo/Group.swift
@@ -0,0 +1,9 @@
+//
+//  Group.swift
+//  Prototype
+//
+//  Created by Brunno Attorre on 10/31/15.
+//  Copyright © 2015 Sztanyi Szabolcs. All rights reserved.
+//
+
+import Foundation
---
+++ Prototype/ZDT-DownloadVideo/Video.swift
@@ -0,0 +1,9 @@
+//
+//  Video.swift
+//  Prototype
+//
+//  Created by Brunno Attorre on 10/31/15.
+//  Copyright © 2015 Sztanyi Szabolcs. All rights reserved.
+//
+
+import Foundation
